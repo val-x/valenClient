6b01b7407da6c8715728198044bd4bacdd874a40
apple <apple@apples-MacBook-Air.local>
2025-01-02 09:58:52 +0530

    0.0.9

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Val-X' }, { name: 'description', content: 'Talk with Val-X, an AI assistant from Joel' }];
 };
 
 export const loader = () => json({});
